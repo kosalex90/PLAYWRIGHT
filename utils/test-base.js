5b08a32c686437d7e1a967de94606a4b2706e6fe
Oleksandr Kostiuchenko <kosalex90@gmail.com>
2022-12-12 19:16:10 +0200

    run formatter

--- utils/test-base.js
+++ utils/test-base.js
@@ -1,18 +1,9 @@
-const base = require('@playwright/test');
+const base = require("@playwright/test");
 
-
-exports.customtest = base.test.extend(
-{
-testDataForOrder :    {
-    username : "anshika@gmail.com",
-    password : "Iamking@000",
-    productName:"Zara Coat 4"
-    }
-
-}
-
-)
-
-
-
-
+exports.customtest = base.test.extend({
+  testDataForOrder: {
+    username: "anshika@gmail.com",
+    password: "Iamking@000",
+    productName: "Zara Coat 4",
+  },
+});
